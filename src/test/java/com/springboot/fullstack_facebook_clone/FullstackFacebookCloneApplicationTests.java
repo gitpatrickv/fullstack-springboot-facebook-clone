3a7fc40029f3de5034156fb9fc9a5fc12cf577e4
rektgit2024 <myemailispatrick@gmail.com>
2024-09-17 07:02:48 +0800

    dockerize app

--- src/test/java/com/springboot/fullstack_facebook_clone/FullstackFacebookCloneApplicationTests.java
+++ src/test/java/com/springboot/fullstack_facebook_clone/FullstackFacebookCloneApplicationTests.java
@@ -1,13 +1,13 @@
-package com.springboot.fullstack_facebook_clone;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FullstackFacebookCloneApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.springboot.fullstack_facebook_clone;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FullstackFacebookCloneApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
